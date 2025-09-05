7fac32d0d6ad5d57dc3a16937bbfe3e491341a81
MarianVentura <marianv1119@gmail.com>
2025-09-05 19:56:30 -0400

    Agregué Room, Navigation y ajustes de Kotlin
    Agregué las dependencias de Room para la base de datos local.
    Agregué las dependencias de Navigation para Compose.
    Agregué el plugin y la biblioteca de serialización de Kotlin.
    Actualicé la versión de Kotlin y la del BOM de Compose.
    Configuré KSP para que funcione con Room.
    Puse la JVM toolchain en la versión 11.
    Agregué opciones de empaquetado para excluir algunos archivos META-INF innecesarios.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,15 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    id("com.google.devtools.ksp")
 }
+
 
 android {
     namespace = "edu.ucne.registrojugadoresmv"
     compileSdk = 36
+
 
     defaultConfig {
         applicationId = "edu.ucne.registrojugadoresmv"
@@ -15,8 +19,10 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
 
     buildTypes {
         release {
@@ -31,15 +37,26 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        jvmToolchain(11) // Or your desired JVM version as an Int
     }
     buildFeatures {
         compose = true
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
+
 dependencies {
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -56,4 +73,18 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+
+    //navigation
+    implementation("androidx.navigation:navigation-compose:2.9.3")
+    implementation(libs.kotlinx.serialization.json)
+
+
+    //room
+    implementation("androidx.room:room-runtime:2.7.2")
+    annotationProcessor("androidx.room:room-compiler:2.7.2")
+    ksp("androidx.room:room-compiler:2.7.2")
+    //  optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:2.7.2")
+
 }
