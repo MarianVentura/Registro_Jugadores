2952577b5bfd910a6504fc0cebacd5c069a1bd2e
MarianVentura <marianv1119@gmail.com>
2025-09-06 19:15:49 -0400

    Trabaje el Use Case GetJugadoresUseCase.kt

---
+++ app/src/main/java/edu/ucne/registrojugadoresmv/domain/usecase/GetJugadoresUseCase.kt
@@ -0,0 +1,14 @@
+// domain/usecase/GetJugadoresUseCase.kt
+package edu.ucne.registrojugadoresmv.domain.usecase
+
+import edu.ucne.registrojugadoresmv.domain.model.Jugador
+import edu.ucne.registrojugadoresmv.domain.repository.JugadorRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetJugadoresUseCase(
+    private val repository: JugadorRepository
+) {
+    operator fun invoke(): Flow<List<Jugador>> {
+        return repository.getAllJugadores()
+    }
+}
