bb5c98453fc2154d271ac362a13e82a4baa999cd
MarianVentura <marianv1119@gmail.com>
2025-09-06 17:08:15 -0400

    Actualizaciones de las librerias :v

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     id("androidx.room") version "2.7.2" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.26" apply false
+    id("com.google.devtools.ksp") version "2.2.10-RC-2.0.2" apply false
 }
