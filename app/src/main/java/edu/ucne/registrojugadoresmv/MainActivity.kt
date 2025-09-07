e12045afe0a8ba08aa1736ed59d5dcf0259f1c17
MarianVentura <marianv1119@gmail.com>
2025-09-06 20:18:05 -0400

    Modifique el MainActivity.kt para que se vea la app que cree y ya no se vea el "Hello Android"

--- app/src/main/java/edu/ucne/registrojugadoresmv/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrojugadoresmv/MainActivity.kt
@@ -4,44 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.registrojugadoresmv.presentation.jugador.JugadorScreen
 import edu.ucne.registrojugadoresmv.ui.theme.Registro_JugadoresTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             Registro_JugadoresTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                JugadorScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Registro_JugadoresTheme {
-        Greeting("Android")
     }
 }
