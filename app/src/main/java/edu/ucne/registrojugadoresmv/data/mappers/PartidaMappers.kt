556d7ac613676dec6ad45604f7c3cffc4857007b
MarianVentura <marianv1119@gmail.com>
2025-09-20 14:52:25 -0400

    Refactorización de PartidaMappers
    - Se cambió el nombre de `toDomain()` a `toModel()` en `PartidaEntity`.
    - Se eliminó `toDomain()` de `PartidaConNombresDto`.
    - Se actualizaron las declaraciones de importación.

--- app/src/main/java/edu/ucne/registrojugadoresmv/data/mappers/PartidaMappers.kt
+++ app/src/main/java/edu/ucne/registrojugadoresmv/data/mappers/PartidaMappers.kt
@@ -1,44 +1,22 @@
 package edu.ucne.registrojugadoresmv.data.mappers
 
-import edu.ucne.registrojugadoresmv.data.local.dao.PartidaConNombresDto
-import edu.ucne.registrojugadoresmv.data.local.entities.Partida as PartidaEntity
-import edu.ucne.registrojugadoresmv.domain.model.Partida as PartidaDomain
-import edu.ucne.registrojugadoresmv.domain.model.PartidaConNombres
+import edu.ucne.registrojugadoresmv.data.local.entities.PartidaEntity
+import edu.ucne.registrojugadoresmv.domain.model.Partida
 
-fun PartidaEntity.toDomain(): PartidaDomain {
-    return PartidaDomain(
-        partidaId = partidaId,
-        fecha = fecha,
-        jugador1Id = jugador1Id,
-        jugador2Id = jugador2Id,
-        ganadorId = ganadorId,
-        esFinalizada = esFinalizada
-    )
-}
+fun PartidaEntity.toModel(): Partida = Partida(
+    partidaId = partidaId,
+    fecha = fecha,
+    jugador1Id = jugador1Id,
+    jugador2Id = jugador2Id,
+    ganadorId = ganadorId,
+    esFinalizada = esFinalizada
+)
 
-fun PartidaDomain.toEntity(): PartidaEntity {
-    return PartidaEntity(
-        partidaId = partidaId,
-        fecha = fecha,
-        jugador1Id = jugador1Id,
-        jugador2Id = jugador2Id,
-        ganadorId = ganadorId,
-        esFinalizada = esFinalizada
-    )
-}
-
-fun PartidaConNombresDto.toDomain(): PartidaConNombres {
-    return PartidaConNombres(
-        partida = PartidaDomain(
-            partidaId = partidaId,
-            fecha = fecha,
-            jugador1Id = jugador1Id,
-            jugador2Id = jugador2Id,
-            ganadorId = ganadorId,
-            esFinalizada = esFinalizada
-        ),
-        jugador1Nombre = jugador1Nombre,
-        jugador2Nombre = jugador2Nombre,
-        ganadorNombre = ganadorNombre
-    )
-}
+fun Partida.toEntity(): PartidaEntity = PartidaEntity(
+    partidaId = partidaId,
+    fecha = fecha,
+    jugador1Id = jugador1Id,
+    jugador2Id = jugador2Id,
+    ganadorId = ganadorId,
+    esFinalizada = esFinalizada
+)
