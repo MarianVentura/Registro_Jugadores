56672f0b4422efdb6a4146444ade3963949c54b8
MarianVentura <marianv1119@gmail.com>
2025-09-20 14:48:34 -0400

    Cree la navegacion de la app
    Cree las rutas Home, Jugadores y Partidas.
    Cree el componente `MainNavigation` que define el `NavHost`.
    Navegacion inicial en la ruta Home.
    Navegacion desde Home a Jugadores y Partidas.

---
+++ app/src/main/java/edu/ucne/registrojugadoresmv/presentation/navigation/MainNavigation.kt
@@ -0,0 +1,42 @@
+package edu.ucne.registrojugadoresmv.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import edu.ucne.registrojugadoresmv.presentation.home.HomeScreen
+import edu.ucne.registrojugadoresmv.presentation.jugador.jugadorScreen.JugadorScreen
+import edu.ucne.registrojugadoresmv.presentation.partida.PartidaScreen
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Jugadores : Screen("jugadores")
+    object Partidas : Screen("partidas")
+}
+
+@Composable
+fun MainNavigation(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(
+                onNavigateToJugadores = {
+                    navController.navigate(Screen.Jugadores.route)
+                },
+                onNavigateToPartidas = {
+                    navController.navigate(Screen.Partidas.route)
+                }
+            )
+        }
+
+        composable(Screen.Jugadores.route) {
+            JugadorScreen()
+        }
+
+        composable(Screen.Partidas.route) {
+            PartidaScreen()
+        }
+    }
+}
