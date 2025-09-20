a197aae218d051931a7f60cc158284a02d26a122
MarianVentura <marianv1119@gmail.com>
2025-09-20 14:50:14 -0400

    Movi PartidaEvent a su propio paquete y actualizo sus eventos
    - Movi la clase PartidaEvent al paquete edu.ucne.registrojugadoresmv.presentation.partida.PartidaEvent.
    - Elimine el evento FechaChanged.
    - Renombre los parámetros de los eventos Jugador1Changed y Jugador2Changed de jugador1Id y jugador2Id a jugadorId.
    - Reordene los eventos EditPartida y SelectPartida.
    - Elimine los eventos LoadJugadores, DismissDeleteDialog y ShowDeleteDialog.

--- app/src/main/java/edu/ucne/registrojugadoresmv/presentation/partida/PartidaEvent/PartidaEvent.kt
+++ app/src/main/java/edu/ucne/registrojugadoresmv/presentation/partida/PartidaEvent/PartidaEvent.kt
@@ -1,23 +1,16 @@
-package edu.ucne.registrojugadoresmv.presentation.partida
+package edu.ucne.registrojugadoresmv.presentation.partida.PartidaEvent
 
 import edu.ucne.registrojugadoresmv.domain.model.Partida
 
 sealed class PartidaEvent {
-    data class FechaChanged(val fecha: String) : PartidaEvent()
-    data class Jugador1Changed(val jugador1Id: Int) : PartidaEvent()
-    data class Jugador2Changed(val jugador2Id: Int) : PartidaEvent()
+    data class Jugador1Changed(val jugadorId: Int) : PartidaEvent()
+    data class Jugador2Changed(val jugadorId: Int) : PartidaEvent()
     data class GanadorChanged(val ganadorId: Int?) : PartidaEvent()
     data class EsFinalizadaChanged(val esFinalizada: Boolean) : PartidaEvent()
-
-    data object SavePartida : PartidaEvent()
-    data object ClearForm : PartidaEvent()
-
+    object SavePartida : PartidaEvent()
+    object ClearForm : PartidaEvent()
     data class DeletePartida(val partidaId: Int) : PartidaEvent()
-    data class SelectPartida(val partidaId: Int) : PartidaEvent()
     data class EditPartida(val partida: Partida) : PartidaEvent()
+    data class SelectPartida(val partidaId: Int) : PartidaEvent()
     data class ConfirmDeletePartida(val partida: Partida) : PartidaEvent()
-
-    data object LoadJugadores : PartidaEvent()
-    data object DismissDeleteDialog : PartidaEvent()
-    data object ShowDeleteDialog : PartidaEvent()
 }
