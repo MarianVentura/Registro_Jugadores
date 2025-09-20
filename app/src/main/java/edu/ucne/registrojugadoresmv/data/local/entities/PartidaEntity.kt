6d99ed1a7c5516a68b961f0b5d419b4f68f2648a
MarianVentura <marianv1119@gmail.com>
2025-09-20 14:50:33 -0400

    Corregi entidad Partida
    - Se cambió el nombre de la clase a PartidaEntity.
    - Se cambió el tipo de dato de la fecha a Date y se inicializó con la fecha actual.
    - Se cambió el tipo de dato de partidaId a Int? y se inicializó con null.
    - Se cambió el tipo de dato de jugador1Id y jugador2Id a Int y se inicializó con 0.
    - Se cambió la acción onDelete de las claves foráneas a RESTRICT.
    - Se cambió el nombre de la entidad Jugador a JugadorEntity en las claves foráneas.

--- app/src/main/java/edu/ucne/registrojugadoresmv/data/local/entities/PartidaEntity.kt
+++ app/src/main/java/edu/ucne/registrojugadoresmv/data/local/entities/PartidaEntity.kt
@@ -3,36 +3,37 @@ package edu.ucne.registrojugadoresmv.data.local.entities
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import java.util.Date
 
 @Entity(
     tableName = "Partidas",
     foreignKeys = [
         ForeignKey(
-            entity = Jugador::class,
+            entity = JugadorEntity::class,
             parentColumns = ["jugadorId"],
             childColumns = ["jugador1Id"],
-            onDelete = ForeignKey.CASCADE
+            onDelete = ForeignKey.RESTRICT
         ),
         ForeignKey(
-            entity = Jugador::class,
+            entity = JugadorEntity::class,
             parentColumns = ["jugadorId"],
             childColumns = ["jugador2Id"],
-            onDelete = ForeignKey.CASCADE
+            onDelete = ForeignKey.RESTRICT
         ),
         ForeignKey(
-            entity = Jugador::class,
+            entity = JugadorEntity::class,
             parentColumns = ["jugadorId"],
             childColumns = ["ganadorId"],
-            onDelete = ForeignKey.SET_NULL
+            onDelete = ForeignKey.RESTRICT
         )
     ]
 )
-data class Partida(
+data class PartidaEntity(
     @PrimaryKey(autoGenerate = true)
-    val partidaId: Int = 0,
-    val fecha: String,
-    val jugador1Id: Int,
-    val jugador2Id: Int,
+    val partidaId: Int? = null,
+    val fecha: Date = Date(),
+    val jugador1Id: Int = 0,
+    val jugador2Id: Int = 0,
     val ganadorId: Int? = null,
     val esFinalizada: Boolean = false
 )
