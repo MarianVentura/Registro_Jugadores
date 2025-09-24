a759ebf7e66955364b60dbb65a8ce31800f699ea
MarianVentura <marianv1119@gmail.com>
2025-09-24 15:00:54 -0400

    Modificaciones en las librerias y gradles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.jetbrains.kotlin.android)
     id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.kotlinx.serialization)
 }
-
 
 android {
     namespace = "edu.ucne.registrojugadoresmv"
     compileSdk = 36
-
 
     defaultConfig {
         applicationId = "edu.ucne.registrojugadoresmv"
@@ -19,10 +18,8 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
 
     buildTypes {
         release {
@@ -33,17 +30,20 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlin {
-        jvmToolchain(17) // Or your desired JVM version as an Int
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
+
     buildFeatures {
         compose = true
     }
-
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
@@ -54,37 +54,57 @@ android {
     }
 }
 
-
 dependencies {
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 
+    //room
+    implementation("androidx.room:room-runtime:2.8.0")
+    implementation(libs.androidx.junit.ktx)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.navigation.compose.android)
+    annotationProcessor("androidx.room:room-compiler:2.8.0")
+    ksp("androidx.room:room-compiler:2.8.0")
+
+    implementation("androidx.compose.foundation:foundation:1.9.1")
+    implementation("androidx.compose.material3:material3:1.3.2")
+
+    //optional
+    implementation("androidx.room:room-ktx:2.8.0")
+    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("androidx.compose.material:material:1.9.1")
+    implementation("androidx.compose.material:material:1.9.1")
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.57.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.57.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+    implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
+
+    // Compose Destinations
+    implementation("io.github.raamcosta.compose-destinations:core:2.2.0")
+    ksp("io.github.raamcosta.compose-destinations:ksp:2.2.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-
-
-    //navigation
-    implementation("androidx.navigation:navigation-compose:2.9.3")
-    implementation(libs.kotlinx.serialization.json)
-
-
-    //room
-    implementation("androidx.room:room-runtime:2.7.2")
-    annotationProcessor("androidx.room:room-compiler:2.7.2")
-    ksp("androidx.room:room-compiler:2.7.2")
-    //  optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:2.7.2")
-
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
