5f3f264b6037456fcff3d46d5f5ef59eef723953
MarianVentura <marianv1119@gmail.com>
2025-10-28 20:26:26 -0400

    Modifique / Actualice JugadorEntity.kt para que soporte sincronizacion remota y capacidades offline.

--- app/src/main/java/edu/ucne/registrojugadoresmv/data/local/entities/JugadorEntity.kt
+++ app/src/main/java/edu/ucne/registrojugadoresmv/data/local/entities/JugadorEntity.kt
@@ -2,11 +2,15 @@ package edu.ucne.registrojugadoresmv.data.local.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 @Entity(tableName = "Jugadores")
 data class JugadorEntity(
-    @PrimaryKey(autoGenerate = true)
-    val jugadorId: Int = 0,
-    val nombres: String = "",
-    val partidas: Int = 0
+    @PrimaryKey
+    val id: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
+    val nombres: String,
+    val email: String,
+    val partidas: Int = 0,
+    val isPendingCreate: Boolean = false
 )
